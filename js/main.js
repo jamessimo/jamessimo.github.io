f0243e6b8e031d7e84d0553c035eb1cd57675ae4
jamessimo <jamessimo89@gmail.com>
2014-06-17 19:10:43 +0800

    New show all effect on HomePage
    + new games content

--- js/main.js
+++ js/main.js
@@ -2,4 +2,26 @@
 	$('#close-shout').click(function(){
 		$(this).parent().toggleClass('hide');
 	});
+	
+	$('#show-all').click(function(){
+		$(this).toggleClass('icon-minus','icon-plus');
+		var frontendList = $(".frontendlist li").toArray();
+		var backendList = $(".backendlist li").toArray();
+		var i = 0;
+		var firstInterval = setInterval(function() { 
+			$(frontendList[i]).toggleClass('show');
+			i++; 
+			if(i >= frontendList.length) clearInterval(firstInterval);
+		}, 100);
+		var j = 0;
+		var secondInterval = setInterval(function() { 
+			$(backendList[j]).toggleClass('show');
+			j++; 
+			if(j >= backendList.length) clearInterval(secondInterval);
+		}, 110);	
+		
+		return false;
+		
+	});
+		  
 })(jQuery);
